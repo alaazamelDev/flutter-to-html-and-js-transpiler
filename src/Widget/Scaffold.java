f94491809e7572e8b0afa7b3d0cebfa5d6036baa
gabsonia_ANAS <anas.reish.77@gmail.com>
2023-01-01 15:18:35 +0300

    widgets 0.5

---
+++ src/Widget/Scaffold.java
@@ -0,0 +1,11 @@
+package Widget;
+
+import properties.Property;
+
+import java.util.List;
+
+public class Scaffold extends Widget{
+    public Scaffold(List<Property> properties) {
+        super("Scaffold", properties);
+    }
+}
