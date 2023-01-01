367d0cf5e70e28871e6abd6dd75c882e9913bd01
AnasDr <me9e98.2012@gmail.com>
2023-01-01 16:50:08 +0300

    border and textField

---
+++ src/Widget/Border.java
@@ -0,0 +1,11 @@
+package Widget;
+
+import properties.Property;
+
+import java.util.List;
+
+public class Border extends Widget{
+    public Border(List<Property> properties) {
+        super("Border", properties);
+    }
+}
