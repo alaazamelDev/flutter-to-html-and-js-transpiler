7f8d7b53627f40ea39398a9557c5e56fda3c1aa5
AnasDr <me9e98.2012@gmail.com>
2023-01-01 15:23:26 +0300

    widgets 1.0

---
+++ src/Widget/AppBar.java
@@ -0,0 +1,11 @@
+package Widget;
+
+import properties.Property;
+
+import java.util.List;
+
+public class AppBar extends Widget{
+    public AppBar(List<Property> properties) {
+        super("AppBar", properties);
+    }
+}
