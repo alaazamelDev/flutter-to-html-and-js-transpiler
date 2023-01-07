9d13bd66eca439817d8e45aba8d10b1f48e9bf74
gabsonia_ANAS <anas.reish.77@gmail.com>
2023-01-07 21:39:16 +0300

    fix util

--- src/utils/UTIL.java
+++ src/utils/UTIL.java
@@ -60,21 +60,6 @@ public class UTIL {
         return false;
     }
 
-    public static String semanticUndeclaredIdentifier(int line, int column, String identifier) {
-        return "Identifier " + identifier + " not declared " + line + ":" + column;
-    }
 
-    public static String semanticTypeMismatch(int line, int column, String expected, String found) {
-        return "type mismatch " + "expected: " + expected + " found:" + found + " " + line + ":" + column;
-    }
-
-
-    public static String semanticUndeclaredIdentifier(int line, int column, String identifier) {
-        return "Identifier " + identifier + " not declared " + line + ":" + column;
-    }
-
-    public static String semanticTypeMismatch(int line, int column, String expected, String found) {
-        return "type mismatch " + "expected: " + expected + " found:" + found + " " + line +":" + column;
-    }
 
 }
