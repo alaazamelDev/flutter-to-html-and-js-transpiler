097800b625e82fa8651fff2ff264e5139be962e9
Alaa Zamel <alaa.zamel23@gmail.com>
2023-06-17 09:27:25 +0300

    fix some errors related to component styling

--- src/visitors/AstToHTML.java
+++ src/visitors/AstToHTML.java
@@ -59,17 +59,17 @@ public class AstToHTML implements Visitor<String> {
         }
 
         // Create div with some styling
-        StringBuilder nav = new StringBuilder("<nav class=\"navbar\" style=\"");
-        nav.append("padding: 0.5rem 1.5rem 0.5rem 1.5rem; ");   // padding attribute
-        nav.append("background-color: #044389; ");   // background color attribute
+        StringBuilder nav = new StringBuilder("<div style=\"");
+        nav.append("padding: 1.0rem; ");   // padding attribute
+        nav.append("background-color: #610F7F; ");   // background color attribute
         nav.append("color: #FFFFFF; ");   // background color attribute
-        nav.append("margin-bottom: 20px; ");
+//        nav.append("margin: 20px; 20px; 20px; 20px;");   // margin attribute
         nav.append(appbarCenterTitleProperty);  // center title attribute
         nav.append(" \">"); // close style attribute
         nav.append("\n");   // break the line
         nav.append(appbarTitleProperty);    // add title attribute as heading tag
         nav.append("\n");
-        nav.append("</nav>");   // close div tag
+        nav.append("</div>");   // close div tag
 
         return nav.toString();
     }
@@ -588,14 +588,14 @@ public class AstToHTML implements Visitor<String> {
         StringBuilder styles = new StringBuilder();
         styles.append("style=\" ");
 
-        StringBuilder hint = new StringBuilder();
+//        StringBuilder hint = new StringBuilder();
 
         for (int i = 0; i < properties.size(); i++) {
             switch (properties.get(i).getName()) {
                 case "label" -> code.append("placeholder=").append(properties.get(i).accept(this)).append(" ");
                 case "value" -> code.append("value=").append(properties.get(i).accept(this)).append(" ");
                 case "textColor", "border", "padding" -> styles.append(properties.get(i).accept(this));
-                case "hint" -> hint.append(properties.get(i).accept(this));
+                case "hint" -> code.append(properties.get(i).accept(this));
                 case "id" -> tagId.append(properties.get(i).accept(this));
                 default -> childIndex = i;
             }
@@ -610,7 +610,7 @@ public class AstToHTML implements Visitor<String> {
 
         code.append(" >\n");
         // append the hint if it's exist, if not it will append empty string
-        code.append(hint);
+//        code.append(hint);
 
         if (childIndex != -1) {
             code.append(properties.get(childIndex).accept(this));
@@ -727,8 +727,8 @@ public class AstToHTML implements Visitor<String> {
     @Override
     public String visit(HintProperty hintProperty) {
         StringBuilder hint = new StringBuilder();
-        hint.append("<h4 style=\"margin-top: 0;\">\n").append(hintProperty.getValue().replace("\"", "")).append("\n").append("</h4>\n");
-
+//        hint.append("<h4 style=\"margin-top: 0;\">\n").append(hintProperty.getValue().replace("\"", "")).append("\n").append("</h4>\n");
+        hint.append("placeholder=").append(hintProperty.getValue());
         return hint.toString();
     }
 
